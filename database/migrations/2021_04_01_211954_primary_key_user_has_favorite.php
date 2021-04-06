10bdf24a4c2b7a633c58fc4bd5031a84385ebd13
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2021-04-06 12:04:13 +0200

    fixing the migration creating twice the primary keys

--- database/migrations/2021_04_01_211954_primary_key_user_has_favorite.php
+++ database/migrations/2021_04_01_211954_primary_key_user_has_favorite.php
@@ -14,7 +14,7 @@ class PrimaryKeyUserHasFavorite extends Migration
     public function up()
     {
         Schema::table('user_has_favorites', function (Blueprint $table) {
-            $table->primary(['user_email', 'course_id']);
+            // $table->primary(['user_email', 'course_id']);
             $table->foreign('user_email')->references('email')->on('users')->OnDelete('cascade');
             $table->foreign('course_id')->references('id')->on('courses')->OnDelete('cascade');
         });
@@ -28,7 +28,7 @@ class PrimaryKeyUserHasFavorite extends Migration
     public function down()
     {
         Schema::table('user_has_favorite', function (Blueprint $table) {
-            $table->dropPrimary(['user_email', 'coure_id']);
+            // $table->dropPrimary(['user_email', 'coure_id']);
             // If migrating throws an exception with sqlite, comment the following line
             $table->dropForeign(['user_email', 'course_id']);
         });
