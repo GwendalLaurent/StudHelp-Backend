ecede8c6b79b2394d3d6d9b2031189a32be21791
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2021-04-08 23:25:43 +0200

    fixing the deployement issue

--- database/migrations/2021_04_08_212140_advertisements_update2.php
+++ database/migrations/2021_04_08_212140_advertisements_update2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class SocialLinksTableUpdate2 extends Migration
+class AdvertisementsUpdate2 extends Migration
 {
     /**
      * Run the migrations.
