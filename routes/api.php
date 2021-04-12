d51a93aa729989993485ba677307fdaa62148700
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2021-04-12 23:39:14 +0200

    changing the route for the firebase_token

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::resource('course', CourseController::class)->only(['index', 'store', 'show']);
     
     Route::put('user/profile/{email}', [UserController::class, 'updateLoginAndName']);
-    Route::put('user/{user_email}/firebase_token', [UserController::class, 'updateFirebaseToken']);
+    Route::put('firebase_token/{user_email}', [UserController::class, 'updateFirebaseToken']);
     Route::resource('user', UserController::class)->only(['index', 'store', 'show'])->parameters([
         'user' => 'email', // setting the email as the parameter for user
     ]);
