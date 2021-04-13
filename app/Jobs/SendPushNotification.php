e11febe3632c63cddc0f944551e2f029c7615ac8
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2021-04-13 14:54:19 +0200

    sending notification to the right users after new ad

--- app/Jobs/SendPushNotification.php
+++ app/Jobs/SendPushNotification.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\DB;
 
 use App\Models\UserHasFavorite;
 use App\Models\User;
+use App\Models\Course;
+
+use Illuminate\Http\Request;
 
 // credits: https://grafikart.fr/tutoriels/jobs-queue-889
 class SendPushNotification implements ShouldQueue
@@ -38,6 +41,7 @@ class SendPushNotification implements ShouldQueue
     public function handle()
     {
         $course_id = $this->advertisement["course_id"];
+        $course_code = Course::find($course_id)["code"];
         
         /* $users = User::select("firebase_token")
                 ->whereRaw("select user_email from user_has_favorites 
@@ -47,9 +51,33 @@ class SendPushNotification implements ShouldQueue
                             select user_email from user_has_favorites 
                                 where users.email = user_has_favorites.user_email and course_id =  {$course_id} )");
 
-        foreach($users as $user){
-            // TODO ask firebase to send the notif to the user
-            echo $user->firebase_token;
-        }
+        $tokens = array_map(function ($value) {
+            return $value->firebase_token;
+        }, $users);
+
+        $server_key = env("FIREBASE_KEY");
+        $headers = [
+            'Authorization: key=' . $server_key,
+            'Content-Type: application/json',
+        ];
+        $data = [
+            "registration_ids" => $tokens,
+            "notification" => [
+            "title" => "Nouvelle annonce!",
+            "body" => "Un utilisateur à posté une nouvelle annonce dans {$course_code}",  
+            ]
+        ];
+        $dataString = json_encode($data);
+        $ch = curl_init();
+    
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+            
+        $response = curl_exec($ch);
+        //dd($response);
     }
 }
