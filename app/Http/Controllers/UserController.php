8aaa12b6bf6856bd05ecc914599144edf28c75d7
Valentin Klein <valentin.klein@student.uclouvain.be>
2021-04-13 16:06:41 +0200

    Deplacement fonction upload image dans userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 
 class UserController extends Controller
@@ -112,6 +112,37 @@ class UserController extends Controller
             ];
             return response($response, 422);
         }
+    }
+
+    public function uploadProfilePicture(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'picture' => 'required|image:jpeg,png,jpg,svg|max:2048'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+        if (!$user){
+            return response([
+            'message' => 'Wrong email'
+            ], 401);
+        }
+        $old_picture = $user->picture;
+        if ($old_picture != NULL){
+            Storage::disk('public')->delete($old_picture);
+        }
+
+        $uploadFolder = 'users';
+        $image = $request->file('picture');
+        $image_uploaded_path = $image->store($uploadFolder, 'public');
+        $uploadedImageResponse = [
+            "image_name" => basename($image_uploaded_path),
+            "image_url" => Storage::disk('public')->url($image_uploaded_path),
+            "mime" => $image->getClientMimeType()
+        ];
+
+        $user->update(['picture' => $image_uploaded_path]);
+        return response($uploadedImageResponse, 200);
     }
     /**
      * Remove the specified resource from storage.
